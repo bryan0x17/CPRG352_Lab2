4636f90843deccca3dfcd8a3d52481f96d27394c
Bryan <50989230+bryan0x17@users.noreply.github.com>
2022-01-13 16:49:31 -0700

    Finish app

--- Calculator/src/main/java/ca/sait/calculator/servlets/ArithmeticCalculatorServlet.java
+++ Calculator/src/main/java/ca/sait/calculator/servlets/ArithmeticCalculatorServlet.java
@@ -36,6 +36,36 @@ public class ArithmeticCalculatorServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        if (request.getParameter("first") != null && request.getParameter("second") != null) {
+            
+            try {
+                String firstString = request.getParameter("first");
+                String secondString = request.getParameter("second");
+                int first = Integer.parseInt(firstString);
+                int second = Integer.parseInt(secondString);
+                int result;
+                if (request.getParameter("calculation").equals("add")) {
+                    result = first + second;
+                } else if (request.getParameter("calculation").equals("subtract")) {
+                    result = first - second;
+                } else if (request.getParameter("calculation").equals("multiply")) {
+                    result = first * second;
+                } else if (request.getParameter("calculation").equals("modulus")) {
+                    result = first % second;
+                } else {
+                    throw new Exception();
+                }
+                request.setAttribute("message", result);
+                request.setAttribute("first", first);
+                request.setAttribute("second", second);
+            } catch (Exception ex) {
+                request.setAttribute("message", "Invalid");
+                log("Exception occured");
+            }
+        } else {
+            request.setAttribute("message", "Invalid");
+            log("Null values");
+        }
         getServletContext().getRequestDispatcher("/WEB-INF/arithmeticcalculator.jsp").forward(request, response);
     }
 }
--- Calculator/src/main/webapp/WEB-INF/arithmeticcalculator.jsp
+++ Calculator/src/main/webapp/WEB-INF/arithmeticcalculator.jsp
@@ -6,16 +6,19 @@
         <title>Arithmetic Calculator</title>
     </head>
     <body>
+        <%
+            session.setAttribute("message", "---");
+        %>
         <h1>Arithmetic Calculator</h1>
         <form action="arithmetic" method="post">
             <label for="first">First</label>
-            <input type="number" name="first" id="first">
+            <input type="number" name="first" id="first" value="${first}">
             <label for="second">Second</label>
-            <input type="number" name="second" id="second">
-            <button type="submit">+</button>
-            <button type="submit">-</button>
-            <button type="submit">*</button>
-            <button type="submit">%</button>
+            <input type="number" name="second" id="second" value="${second}">
+            <button type="submit" name="calculation" value="add">+</button>
+            <button type="submit" name="calculation" value="subtract">-</button>
+            <button type="submit" name="calculation" value="multiply">*</button>
+            <button type="submit" name="calculation" value="modulus">%</button>
             <p>Result: ${message}</p>
             <a href="age">Age Calculator</a>
             
